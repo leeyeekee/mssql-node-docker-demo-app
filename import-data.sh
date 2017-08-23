f2b4505bf32c11a855ad1a3f3c23bc124c9c3f3b
leeyeekee <leeyeekee@hotmail.com>
2017-08-23 16:29:47 +0800

    Update import-data.sh

--- import-data.sh
+++ import-data.sh
@@ -2,7 +2,7 @@
 sleep 90s
 
 #run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S $HOSTNAME -U sa -P $MSSQL_SA_PASSWORD -d master -i setup.sql
+/opt/mssql-tools/bin/sqlcmd -S $HOSTNAME -U sa -P D1toso1234! -d master -i setup.sql
 
 #import the data from the csv file
-/opt/mssql-tools/bin/bcp DemoData.dbo.Products in "/usr/src/app/Products.csv" -c -t',' -S $HOSTNAME -U sa -P $MSSQL_SA_PASSWORD
+/opt/mssql-tools/bin/bcp DemoData.dbo.Products in "/usr/src/app/Products.csv" -c -t',' -S $HOSTNAME -U sa -P D1toso1234!
