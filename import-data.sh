a7e9a72a4d861c5f8cc41cf2c02f1b320c957d6d
leeyeekee <leeyeekee@hotmail.com>
2017-08-23 15:29:12 +0800

    Update import-data.sh

--- import-data.sh
+++ import-data.sh
@@ -2,7 +2,7 @@
 sleep 90s
 
 #run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Yukon900 -d master -i setup.sql
+/opt/mssql-tools/bin/sqlcmd -S $HOSTNAME -U sa -P $MSSQL_SA_PASSWORD -d master -i setup.sql
 
 #import the data from the csv file
-/opt/mssql-tools/bin/bcp DemoData.dbo.Products in "/usr/src/app/Products.csv" -c -t',' -S localhost -U sa -P Yukon900
+/opt/mssql-tools/bin/bcp DemoData.dbo.Products in "/usr/src/app/Products.csv" -c -t',' -S $HOSTNAME -U sa -P $MSSQL_SA_PASSWORD
