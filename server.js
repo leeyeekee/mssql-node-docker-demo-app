46f8bd8d77333f179a633373d1374724577c5a52
leeyeekee <leeyeekee@hotmail.com>
2017-08-24 17:51:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', function (req, res) {
     var config = {
         userName: 'sa',
         password: 'D1toso1234!', // update me
-        server: 'mssql-node-docker-demo-app',
+        server: 'mssql-node-docker-demo-app.demo.svc',
         options: {
             database: 'DemoData'
         }
